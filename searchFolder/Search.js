4484e00a27851d453882e19d05cd8e5272e14334
ChandanChowdhury514 <103625529+ChandanChowdhury514@users.noreply.github.com>
2022-05-10 21:05:45 +0530

    initial commit

---
+++ searchFolder/Search.js
@@ -0,0 +1,30 @@
+import  {useState} from 'react';
+import TextField from "@mui/material/TextField";
+import List from './List';
+import './Style.css';
+
+const Search = () => {
+    const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    const lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+  return (
+    <div className='main'>
+    <h1>React Search</h1>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          onChange={inputHandler}
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
+        <List input={inputText}/>
+    </div>
+  )
+}
+
+export default Search
